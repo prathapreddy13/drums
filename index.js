ec662a86c966afad5cd86707ed86603991dcbf14
prathapreddy13 <59243467+prathapreddy13@users.noreply.github.com>
2021-10-30 08:08:06 +0530

    init commit
    none

---
+++ index.js
@@ -0,0 +1,59 @@
+var len=document.querySelectorAll(".drum").length;
+for(var i=0;i<len;i++)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+    playSound(this.innerHTML);
+    addAnimation(this.innerHTML);
+  });
+}
+document.addEventListener("keypress",function(event){
+  playSound(event.key);
+  addAnimation(event.key);
+});
+function playSound(key)
+{
+  switch(key)
+  {
+    case "w":
+      var tom1=new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom1=new Audio("sounds/tom-2.mp3");
+      tom1.play();
+      break;
+    case "s":
+      var tom1=new Audio("sounds/tom-3.mp3");
+      tom1.play();
+      break;
+    case "d":
+      var tom1=new Audio("sounds/tom-4.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom1=new Audio("sounds/crash.mp3");
+      tom1.play();
+      break;
+    case "k":
+      var tom1=new Audio("sounds/snare.mp3");
+      tom1.play();
+      break;
+    case "l":
+      var tom1=new Audio("sounds/kick-bass.mp3");
+      tom1.play();
+      break;
+    default:
+      console.log(key);
+
+  }
+}
+function addAnimation(key)
+{
+  var obj=document.querySelector("."+key);
+  obj.classList.add("pressed");
+
+  setTimeout(function(){
+    obj.classList.remove("pressed");
+  },100);
+
+}
